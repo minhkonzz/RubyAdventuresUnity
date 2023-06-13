1440cff232382d1d21602736aeb4c67664f38054
minhkonzz <minhphm37@gmail.com>
2023-06-13 13:40:07 +0700

    update 13-06-2023 01:40PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,27 +3,57 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-
+   public float speed = 3.0f; 
    public int maxHealth = 5; 
    int currentHealth;
    Rigidbody2D rigidbody2d;
+
+   bool isInvincible;
+   float invincibleTimer;
+
+   float horizontal, vertical;
+
+   Animator animator;
+   Vector2 lookDir = new Vector2(1, 0); 
 
    void Start()
    {
       currentHealth = maxHealth;
       rigidbody2d = GetComponent<Rigidbody2D>();
+      animator = GetComponent<Animator>();
+
       QualitySettings.vSyncCount = 0; 
       Application.targetFrameRate = 60;
    }
 
    void Update()
    {	
-      float horizontal = Input.GetAxis("Horizontal"); 
-      float vertical = Input.GetAxis("Vertical"); 
-      Vector2 position = rigidbody2d.position;  
-      position.x += 5.0f * horizontal * Time.deltaTime; 
-      position.y += 5.0f * vertical * Time.deltaTime;
-      transform.position = position;
+      horizontal = Input.GetAxis("Horizontal"); 
+      vertical = Input.GetAxis("Vertical");
+
+      Vector2 move = new Vector2(horizontal, vertical);
+
+      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+      {
+         lookDir.Set(move.x, move.y);
+         lookDir.Normalize();
+      } 
+
+      animator.SetFloat("Horizontal", lookDir.x); 
+      animator.SetFloat("Vertical", lookDir.y);
+      animator.SetFloat("Speed", move.magnitude);
+
+      if (isInvincible)
+      {
+         invincibleTimer -= Time.deltaTime;
+         if (invincibleTimer < 0) isInvincible = false;
+      }  
+   }
+
+   void FixedUpdate() {
+      Vector2 position = rigidbody2d.position;
+      position.x = position.x + speed * horizontal * Time.deltaTime;
+      position.y = position.y + speed * vertical * Time.deltaTime;
 
       rigidbody2d.MovePosition(position);
    }
---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthBar : MonoBehaviour
+{
+
+    public Image mask;
+    float originalSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Debug.Log(originalSize);
+    }
+
+    public void SetValue(float value) {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
